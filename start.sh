b7b6dac9668050bea273c301ffbbc7a02387d58c
stevekibe <stevekibe2345@gmail.com>
2018-09-16 12:04:56 +0300

    add the code in the start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+export DATABASE_URL='postgres://mrwpegcpdyxfda:74d21e087a58b2c7f08af9c7462e11eebb118b7099e333151ee8465ebcbd579e@ec2-23-23-92-179.compute-1.amazonaws.com:5432/df9v0ohq12rn5a'
+export SECRET_KEY ='steve'
+python3.6 manage.py server
