e8ba73b2456f449e2736fe9577b6b4e349574e5b
stevekibe <stevekibe2345@gmail.com>
2018-09-16 11:54:28 +0300

    add the code in the config file

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+bootsrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app(config_name):
+    '''
+    method for configuring the app
+    '''
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+    #initializing the flask extensions
+    bootsrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    #Regestering blueprints
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+
+    return app
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/steve/Documents/Python/personal-blog/virtual"
+set -gx VIRTUAL_ENV "/home/steve/Documents/Python/One-minite/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
