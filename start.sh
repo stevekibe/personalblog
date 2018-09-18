f81cf9d8572a0d12881bcd148ea3c821618dc7b6
stevekibe <stevekibe2345@gmail.com>
2018-09-18 13:35:56 +0300

    deploy

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-
+export DATABASE_URL='postgres://vcdpjhcltcbvsj:f4fcb62adbf1a08a10cfda03af63c32db5ff5b802d41ed972c40b86bc5c75f70@ec2-174-129-32-37.compute-1.amazonaws.com:5432/d13p56kkggf497'
 export SECRET_KEY=steve
 python3.6 manage.py server
